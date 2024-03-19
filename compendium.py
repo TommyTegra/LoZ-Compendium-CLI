bd550d81f485c9455c640091223f784f5b77865f
TommyTegra <tommyhnguyen729@gmail.com>
2024-03-19 02:44:17 -0500

    Completed Hyrule Compendium CLI

--- compendium.py
+++ compendium.py
@@ -8,6 +8,16 @@ import requests
 import typer
 from InquirerPy import inquirer
 import pyfiglet
+import zmq
+from PIL import Image
+from io import BytesIO
+
+# ZeroMQ object
+obj = zmq.Context()
+socket = obj.socket(zmq.REQ)
+
+# Connect to Microservice server
+socket.connect("tcp://localhost:5555")
 
 
 def home():
@@ -57,17 +67,23 @@ def task_selection(game_ver):
         message=f'({game_ver[1]}) Please select a service: ',
         choices=[
             'ID Lookup (requires exact number)\n        -Enter an ID number to get entry',
+            'Name Lookup (requires exact full name)\n        -Enter name to get entry',
             'Explore Categories (browse manually)\n        '
             '-Lists names of all entries within a selected category',
+            'Get random compendium entry',
             'Back (to home page)',
             'Exit (closes program)'
         ],
     ).execute()
     if task_choice == 'ID Lookup (requires exact number)\n        -Enter an ID number to get entry':
         id_lookup(game_ver)
+    elif task_choice == 'Name Lookup (requires exact full name)\n        -Enter name to get entry':
+        name_lookup(game_ver)
     elif task_choice == 'Explore Categories (browse manually)\n        ' \
                         '-Lists names of all entries within a selected category':
         explore_cat(game_ver)
+    elif task_choice == 'Get random compendium entry':
+        random_entry(game_ver)
     elif task_choice == 'Back (to home page)':
         home()
     else:
@@ -88,12 +104,17 @@ def id_lookup(game_ver):
         default=None,
         mandatory=False
     ).execute()
+    results = requests.get(f'https://botw-compendium.herokuapp.com/api/v3/compendium/entry/{id_num}?game={game_ver[0]}')
+    entry = results.json()
+    if not entry["data"]:
+        id_num = None
     if id_num is not None:
         result(game_ver, id_num)
     alt_choice = inquirer.select(
-        message='Input cancelled.',
+        message='Invalid input or input cancelled.',
         choices=[
             'Retry ID input',
+            'Try name input',
             'Manually search (go to explore)',
             'Back (to task selection)',
             'Start over (to home page)',
@@ -101,6 +122,54 @@ def id_lookup(game_ver):
         ],
     ).execute()
     if alt_choice == 'Retry ID input':
+        id_lookup(game_ver)
+    elif alt_choice == 'Try name input':
+        name_lookup(game_ver)
+    elif alt_choice == 'Manually search (go to explore)':
+        explore_cat(game_ver)
+    elif alt_choice == 'Back (to task selection)':
+        task_selection(game_ver)
+    elif alt_choice == 'Start over (to home page)':
+        home()
+    else:
+        quit()
+
+
+def name_lookup(game_ver):
+    """
+    Name lookup page. Prompts the user for a string which is the name
+    of the potential entry. Will return entry on valid input, give
+    alternate choices otherwise.
+    """
+    print('--------------------------------------------------'
+          '--------------------------------------------------')
+    print(f'({game_ver[1]}) Name Lookup: ')
+    print('You may use Ctrl + Z for alternate choices.\n')
+    id_num = inquirer.text(
+        message='Please enter an ID number: ',
+        default="",
+        mandatory=False
+    ).execute()
+    results = requests.get(f'https://botw-compendium.herokuapp.com/api/v3/compendium/entry/{id_num}?game={game_ver[0]}')
+    entry = results.json()
+    if not entry["data"]:
+        id_num = ""
+    if id_num != "":
+        result(game_ver, id_num)
+    alt_choice = inquirer.select(
+        message='Invalid input or input cancelled.',
+        choices=[
+            'Retry name input',
+            'Try ID input',
+            'Manually search (go to explore)',
+            'Back (to task selection)',
+            'Start over (to home page)',
+            'Exit (close program)'
+        ],
+    ).execute()
+    if alt_choice == "Retry name input":
+        name_lookup(game_ver)
+    if alt_choice == 'Try ID input':
         id_lookup(game_ver)
     elif alt_choice == 'Manually search (go to explore)':
         explore_cat(game_ver)
@@ -110,6 +179,7 @@ def id_lookup(game_ver):
         home()
     else:
         quit()
+
 
 
 def explore_cat(game_ver):
@@ -165,6 +235,36 @@ def explore_cat(game_ver):
         quit()
 
 
+def random_entry(game_ver):
+    """
+    Gets a random entry from the compendium. Call microservice for
+    random ID number.
+    """
+    print('--------------------------------------------------'
+          '--------------------------------------------------')
+    ms_msg = str(game_ver).encode()
+    socket.send(ms_msg)
+    receive = socket.recv()
+    rand_id = receive.decode()
+    confirm_choice = inquirer.select(
+        message='This will give you a random entry.\n Confirm?',
+        choices=[
+            "Yes",
+            "No (to task selection)",
+            "Start over (to home page)",
+            "Exit (close program)"
+        ],
+    ).execute()
+    if confirm_choice == "Yes":
+        result(game_ver, rand_id)
+    elif confirm_choice == "No (to task selection)":
+        task_selection(game_ver)
+    elif confirm_choice == "Start over (to home page)":
+        home()
+    else:
+        quit()
+
+
 def result(game_ver, id_num):
     """
     Results page. Prints the ID number, name, and the description.
@@ -178,6 +278,29 @@ def result(game_ver, id_num):
           f'ID number: {data["id"]} \n'
           f'Name: {data["name"]} \n'
           f'Description: {data["description"]}\n')
+
+    if game_ver[0] == 1:
+        image_choice = inquirer.select(
+            message='Do you want the image?',
+            choices=[
+                'I want the image.',
+                'I do not want the image.'
+            ],
+        ).execute()
+        if image_choice == 'I want the image.':
+            confirm = inquirer.confirm(
+                message="Are you sure? It will open the image in a new window."
+            ).execute()
+            if confirm:
+                get_image(id_num)
+
+    whole_entry(game_ver, data)
+
+
+def whole_entry(game_ver, data):
+    """
+    Inquires about getting the entire entry information.
+    """
 
     all_choice = inquirer.select(
         message='Do you want the whole available entry?',
@@ -207,6 +330,19 @@ def result(game_ver, id_num):
         home()
     else:
         quit()
+
+
+def get_image(id_num):
+    """
+    Inquires about obtain entry image.
+    """
+    response = requests.get(f'https://botw-compendium.herokuapp.com/api/v3/compendium/entry/{id_num}/image')
+    # Open the image using PIL
+    image = Image.open(BytesIO(response.content))
+
+    # Display the image
+    image.show()
+    return
 
 
 if __name__ == '__main__':
